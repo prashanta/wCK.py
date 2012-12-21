a8a9c6bb06dded27092c494a1851deda47d6d728
Prashanta <pmshrestha@gmail.com>
2012-12-21 08:51:49 +0700

    Converted to python class

--- wck.py
+++ wck.py
@@ -2,46 +2,50 @@ import sys
 import serial
 import struct
 
-def test():
-	print "Hello"
+class skcw:
+	port = "/dev/tty.usbmodem24121"
+	ser = 0
+	def __init__(self, port):
+	    self.data = []
+	    if port:
+	    	self.port = port
+		
+	# Move servo to specified target position (1-254) 
+	
+	
+	def pos(self, torque, target):
+		header =  255  	
+		data = (torque<<5) | 0
+		chksum = (data ^ target) & 0x7F
+		print hex(header) + " " + hex(data) + " " + hex(target) + " " + hex(chksum)
+		self.ser = serial.Serial(self.port,115200,timeout=1)	
+		ser.write(struct.pack("<B",header))
+		ser.write(struct.pack("<B",data))
+		ser.write(struct.pack("<B",target))
+		ser.write(struct.pack("<B",chksum))
 
-# Move servo to specified target position (1-254) 
-def pos(torque, target):
-  	ser = serial.Serial("/dev/tty.usbmodem411",115200,timeout=1)
-  	header =  255  	
-  	data = (torque<<5) | 0
-  	chksum = (data ^ target) & 0x7F
-  	print hex(header) + " " + hex(data) + " " + hex(target) + " " + hex(chksum)
-  	ser.write(struct.pack("<B",header))
-  	ser.write(struct.pack("<B",data))
-  	ser.write(struct.pack("<B",target))
-  	ser.write(struct.pack("<B",chksum))
-  	ser.close()
-  	
-# Rotate wheel in cloclwise direction at given speed  	
-def cw(speed):
-  	ser = serial.Serial("/dev/tty.usbmodem411",115200,timeout=1)
-  	header =  255  	
-  	data1 = (6<<5) | 0
-  	data2= (4<<4)| speed
-  	chksum = (data1 ^ data2) & 0x7F
-  	print hex(header) + " " + hex(data1) + " " + hex(data2) + " " + hex(chksum)
-  	ser.write(struct.pack("<B",header))
-  	ser.write(struct.pack("<B",data1))
-  	ser.write(struct.pack("<B",data2))
-  	ser.write(struct.pack("<B",chksum))
-  	ser.close()  	
-
-# Rotate wheel in anti-cloclwise direction at given speed  	
-def ccw(speed):
-  	ser = serial.Serial("/dev/tty.usbmodem411",115200,timeout=1)
-  	header =  255  	
-  	data1 = (6<<5) | 0
-  	data2= (3<<4)| speed
-  	chksum = (data1 ^ data2) & 0x7F
-  	print hex(header) + " " + hex(data1) + " " + hex(data2) + " " + hex(chksum)
-  	ser.write(struct.pack("<B",header))
-  	ser.write(struct.pack("<B",data1))
-  	ser.write(struct.pack("<B",data2))
-  	ser.write(struct.pack("<B",chksum))
-  	ser.close()  	 
+		
+	# Rotate wheel in cloclwise direction at given speed  	
+	def cw(self, speed):
+		ser = serial.Serial(self.port,115200,timeout=1)
+		header =  255  	
+		data1 = (6<<5) | 0
+		data2= (4<<4)| speed
+		chksum = (data1 ^ data2) & 0x7F
+		print hex(header) + " " + hex(data1) + " " + hex(data2) + " " + hex(chksum)
+		self.ser.write(struct.pack("<B",header))
+		self.ser.write(struct.pack("<B",data1))
+		self.ser.write(struct.pack("<B",data2))
+		self.ser.write(struct.pack("<B",chksum))
+	
+	# Rotate wheel in anti-cloclwise direction at given speed  	
+	def ccw(self, speed):
+		header =  255  	
+		data1 = (6<<5) | 0
+		data2= (3<<4)| speed
+		chksum = (data1 ^ data2) & 0x7F
+		print hex(header) + " " + hex(data1) + " " + hex(data2) + " " + hex(chksum)
+		self.ser.write(struct.pack("<B",header))
+		self.ser.write(struct.pack("<B",data1))
+		self.ser.write(struct.pack("<B",data2))
+		self.ser.write(struct.pack("<B",chksum))
